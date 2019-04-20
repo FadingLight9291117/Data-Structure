8916670982e48422ea63d6487a004cfdf23d3e68
Liangzhao Che <FadingLight9291117@126.com>
2019-04-21 05:22:54 +0800

    单链表添加split方法

--- LinkList/testLinkList.cpp
+++ LinkList/testLinkList.cpp
@@ -39,11 +39,22 @@ void testReverse() {
 	reverse(L);
 	DispList(L);
 }
+void testSplit()
+{
+	int a[8] = { 1,2,3,4,5,6,7,8 };
+	LinkList* L, * L1, * L2;
+	CreateListR(L, a, 8);
+	DispList(L);
+	split(L, L1, L2);
+	DispList(L1);
+	DispList(L2);
+}
 int main()
 {
 	//testDelmaxnode();
 	//testSort();
-	testReverse();
+	//testReverse();
+	testSplit();
 	return 0;
 }
 
